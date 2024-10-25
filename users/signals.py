9c176ea0ed8738c8c0392eb25a82fd11435b1acb
Umar Hoshimov <umarkhashimov05@gmail.com>
2024-10-25 17:36:01 +0500

    hash use password while creating

---
+++ users/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from datetime import timedelta
+from .models import UsersModel
+
+@receiver(post_save, sender=UsersModel)
+def create_sessions(sender, instance, created, **kwargs):
+    if created:
+        pswd = instance.password
+        user = UsersModel.objects.get(id=instance.id)
+        user.set_password(pswd)
+        user.save()
