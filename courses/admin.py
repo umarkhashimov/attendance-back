e88287db1d45dc980d3375cbcd6ab3c345b1d1da
Umar Hoshimov <umarkhashimov05@gmail.com>
2024-10-29 11:47:02 +0500

    courses model update concept

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CourseModel, SessionsModel
+from .models import CourseModel, SessionsModel, SubjectModel
 
 admin.site.register(CourseModel)
 admin.site.register(SessionsModel)
+admin.site.register(SubjectModel)
--- courses/models.py
+++ courses/models.py
@@ -1,20 +1,42 @@
 from django.db import models
 from django.conf import settings  # to use custom user model if needed
 from users.models import UsersModel
+from multiselectfield import MultiSelectField
 
 
+class SubjectModel(models.Model):
+    name = models.CharField(max_length=150, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "subject"
+        verbose_name_plural = "subjects"
+
+WEEKDAY_CHOICES = [
+    ('1', 'Понедельник'),
+    ('2', 'Вторник'),
+    ('3', 'Среда'),
+    ('4', 'Четверг'),
+    ('5', 'Пятница'),
+    ('6', 'Суббота'),
+]
 
 class CourseModel(models.Model):
     course_name = models.CharField(max_length=100, unique=True)
-    subject = models.CharField(max_length=100, blank=True, null=True)
+    weekdays = MultiSelectField(choices=WEEKDAY_CHOICES)
+    subject = models.ForeignKey(SubjectModel, on_delete=models.CASCADE)
     teacher = models.ForeignKey(UsersModel, on_delete=models.SET_NULL, null=True, limit_choices_to={'role': '1'})
     start_date = models.DateField()
     lesson_time = models.TimeField()
-    duration = models.TimeField()
+    duration = models.PositiveIntegerField()
     total_lessons = models.PositiveIntegerField()
-    is_started = models.BooleanField(default=False)
     course_cost = models.PositiveIntegerField(default=0)
+    is_started = models.BooleanField(default=False)
+    finished = models.BooleanField(default=False)
+
+    
     
     def __str__(self):
         return self.course_name
@@ -40,3 +62,5 @@ class SessionsModel(models.Model):
         verbose_name_plural = 'sessions'
         unique_together = ('course', 'session_number')
         ordering = ['date']
+
+
