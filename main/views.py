6f8b89538a286ca10edffeef9b19c19f8d99ae2f
Umar Hoshimov <umarkhashimov05@gmail.com>
2024-10-31 00:43:39 +0500

    lessons today & main page update

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, DetailView
 from django.core.exceptions import PermissionDenied
-
+from datetime import date
+from django.utils import timezone
 from courses.models import CourseModel, SessionsModel
 
 class MainPageView(TemplateView):
@@ -8,10 +9,17 @@ class MainPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        today = date.today()
+        sessions_today = SessionsModel.objects.filter(date=today)
         if self.request.user.role == '1':
-            context["courses"] = CourseModel.objects.all().filter(teacher__id=self.request.user.id)
+            courses = CourseModel.objects.all().filter(teacher__id=self.request.user.id)
+            courses_with_today_sessions = CourseModel.objects.filter(teacher__id=self.request.user.id, id__in=sessions_today.values_list('course_id', flat=True)).distinct()
         else:
-            context["courses"] = CourseModel.objects.all()
+            courses = CourseModel.objects.all()
+            courses_with_today_sessions = CourseModel.objects.filter(id__in=sessions_today.values_list('course_id', flat=True)).distinct()
+
+        context["courses"] = courses
+        context['sessions_today'] = courses_with_today_sessions
         return context
     
 
