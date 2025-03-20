89c153100d7c05281a4342876decd947e2d3d1a0
Umar Hoshimov <umarkhashimov05@gmail.com>
2025-03-20 12:18:46 +0500

    ...

--- payment/views.py
+++ payment/views.py
@@ -17,7 +17,7 @@ class PaymentsListView(ListView):
     template_name = 'payment/payments_list.html'
     context_object_name = 'payments'
     ordering = ['-id']
-    paginate_by = 2
+    paginate_by = 30
 
 class CreatePaymentView(View):
 
