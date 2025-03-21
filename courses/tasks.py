caf80f3333314af0edf32c67d844cf8def8351be
Umar Hoshimov <umarkhashimov05@gmail.com>
2025-03-22 02:49:30 +0500

    testing

--- courses/tasks.py
+++ courses/tasks.py
@@ -54,7 +54,7 @@ def start():
     if os.environ.get('RUN_MAIN') == 'true':
         scheduler = BackgroundScheduler()
         scheduler.add_job(mark_unmarked_sessions, CronTrigger(hour=23, minute=00))  # 11 PM
-        scheduler.add_job(send_test_message, IntervalTrigger(seconds=10), max_instances=1)  # 11 PM
-        scheduler.add_job(send_test_message, CronTrigger(hour=8, minute=00))  # 11 PM
+        # scheduler.add_job(send_test_message, IntervalTrigger(seconds=10), max_instances=1)  # 11 PM
+        scheduler.add_job(send_test_message, CronTrigger(hour=3, minute=1))  # 11 PM
         scheduler.start()
 
