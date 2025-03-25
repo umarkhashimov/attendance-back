cd29faf5b46e66f9b78bc2f3f4df3dfc09235948
Umar Hoshimov <umarkhashimov05@gmail.com>
2025-03-25 14:33:40 +0500

    active students counter

--- main/views.py
+++ main/views.py
@@ -91,6 +91,7 @@ class StudentsListView(AdminRequired, ListView):
         context['filter_form'] = StudentsListFilterForm(self.request.GET)
         context['create_student_form'] = StudentInfoForm
         context['queryset_length'] = self.get_queryset().count()
+        context['active_students_count'] = StudentModel.objects.filter(enrollment__status=True).distinct().count()
         return context
 
     def get_queryset(self):
