05c340fff988a6cadfd824193018d9522e0ec3f4
Umar Khashimov <umarkhashimov05@gmail.com>
2025-08-31 01:06:02 +0500

    fix

--- analytics/tasks.py
+++ analytics/tasks.py
@@ -23,7 +23,8 @@ def record_daily_analytics(day=None):
         enrollments = Enrollment.objects.filter(status=True, trial_lesson=False,
                                                 payment_due__isnull=False).distinct().count()
         trial_enrollments = Enrollment.objects.filter(status=True, trial_lesson=True).distinct().count()
-        payments_qs = PaymentModel.objects.filter(date__day=today.day, date__month=today.month, date__year=today.year)
+        payments_qs = PaymentModel.objects.filter(date__date=today)
+        print(payments_qs)
         payments_sum = payments_qs.aggregate(sum=Sum("amount"))['sum']
         print(payments_sum, type(payments_sum))
         new_students = StudentModel.objects.filter(archived=False, enrollment_date=today).count()
