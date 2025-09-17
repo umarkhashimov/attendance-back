597fbf555817936556948bee48dbaadeea05b4d0
Umar Khashimov <umarkhashimov05@gmail.com>
2025-09-17 11:47:28 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'users.middleware.AuthRequiredMiddleware',
+    'users.middleware.AuthRequiredMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
