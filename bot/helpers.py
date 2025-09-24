b05fb2cf5a31035dba45368411ea1b89d100187f
Umar Khashimov <umarkhashimov05@gmail.com>
2025-09-25 01:00:36 +0500

    ...

--- bot/helpers.py
+++ bot/helpers.py
@@ -77,7 +77,7 @@ def get_subjects():
 def get_subject_teachers(subject_id):
     close_old_connections()
 
-    teachers = UsersModel.objects.filter(role='1', coursemodel__subject_id=subject_id, bio__isnull=False, image__isnull=False, first_name__isnull=False, last_name__isnull=False).distinct()
+    teachers = UsersModel.objects.filter(role='1', coursemodel__subject_id=subject_id).exclude(bio__isnull=True, image__isnull=True, first_name__isnull=True, last_name__isnull=True).distinct()
 
     return [{'id': teacher.id,'fname': teacher.first_name, 'lname': teacher.last_name} for teacher in teachers]
 
