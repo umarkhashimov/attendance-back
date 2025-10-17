39ff216415b2e0ab21e6c1532fb1fdc788f8bb76
Umar Khashimov <umarkhashimov05@gmail.com>
2025-10-17 18:46:58 +0500

    update

--- config/helpers.py
+++ config/helpers.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 class DefaultPageSize(PageNumberPagination):
     page_query_param = 'p'
-    page_size = 2  # items per page
+    page_size = 30  # items per page
     # page_size_query_param = 'page_size'  # allow ?page_size=20 in URL
     # max_page_size = 100
